b495e7009d5370c6b3858167b56ec9e5e2d8ef04
Misha Maniuk <mikhail.maniuk@gmail.com>
2018-10-01 16:07:34 +0300

    zeros task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+
+  let powValue = 0, 
+      powNumber = 1,
+      zeroNumber = 0;
+
+  while(powValue <= number){
+    powValue = Math.pow(5, powNumber)
+    powNumber++
+  }
+
+  for (let i = 1; i < powNumber - 1; i++) {
+    let iterationResult = Math.floor(number / Math.pow(5, i));
+    zeroNumber = zeroNumber + iterationResult;
+  }
+  return zeroNumber
 }
